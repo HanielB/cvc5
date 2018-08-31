9f5043f76b1b117c3d32cf205c3f454710dac89c
Haniel Barbosa <hanielbbarbosa@gmail.com>
2018-08-31 12:23:21 -0500

    renabling disabled options

--- src/theory/quantifiers/sygus/sygus_repair_const.cpp
+++ src/theory/quantifiers/sygus/sygus_repair_const.cpp
@@ -271,6 +271,11 @@ bool SygusRepairConst::repairSolution(const std::vector<Node>& candidates,
       repcChecker.reset(new SmtEngine(&em));
       repcChecker->setTimeLimit(options::sygusRepairConstTimeout(), true);
       repcChecker->setLogic(smt::currentSmtEngine()->getLogicInfo());
+      // renable disable options by sygus
+      repcChecker->setOption("miniscope-quant", true);
+      repcChecker->setOption("miniscope-quant-fv", true);
+      repcChecker->setOption("quant-split", true);
+      // export
       Expr e_fo_body = fo_body.toExpr().exportTo(&em, varMap);
       repcChecker->assertFormula(e_fo_body);
       r = repcChecker->checkSat();
