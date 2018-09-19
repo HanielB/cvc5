13d1c5b82d889fec54cd7353c6d124c7679c5836
Haniel Babosa <hanielbbarbosa@gmail.com>
2018-09-19 18:01:44 -0500

    starting sygus printer

---
+++ src/printer/sygus/sygus_printer.h
@@ -0,0 +1,62 @@
+/*********************                                                        */
+/*! \file sygus_printer.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Haniel Barbosa
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief The pretty-printer interface for the SyGuS output language
+ **
+ ** The pretty-printer interface for the SyGuS output language.
+ **/
+
+#include "cvc4_private.h"
+
+#ifndef __CVC4__PRINTER__SYGUS_PRINTER_H
+#define __CVC4__PRINTER__SYGUS_PRINTER_H
+
+#include <iostream>
+
+#include "printer/printer.h"
+
+namespace CVC4 {
+namespace printer {
+namespace sygus {
+
+class SygusPrinter : public CVC4::Printer
+{
+ public:
+  using CVC4::Printer::toStream;
+
+  void toStream(std::ostream& out,
+                TNode n,
+                int toDepth,
+                bool types,
+                size_t dag) const override;
+
+  void toStream(std::ostream& out,
+                const Command* c,
+                int toDepth,
+                bool types,
+                size_t dag) const override;
+
+  void toStream(std::ostream& out, const CommandStatus* s) const override;
+
+ private:
+  void toStream(
+      std::ostream& out, TNode n, int toDepth, bool types, TypeNode nt) const;
+  void toStream(std::ostream& out,
+                const Model& m,
+                const Command* c) const override;
+  void toStream(std::ostream& out, const SExpr& sexpr) const;
+}; /* class SygusPrinter */
+
+}  // namespace sygus
+}  // namespace printer
+}  // namespace CVC4
+
+#endif /* __CVC4__PRINTER__SYGUS_PRINTER_H */
