f3e708bdfc9ac9c0be98a5e81ae2807fa51298b4
Haniel Babosa <hanielbbarbosa@gmail.com>
2018-09-19 18:02:54 -0500

    more

--- src/printer/sygus/sygus_printer.cpp
+++ src/printer/sygus/sygus_printer.cpp
@@ -2,7 +2,7 @@
 /*! \file sygus_printer.cpp
  ** \verbatim
  ** Top contributors (to current version):
- **   Haniel B
+ **   Haniel Barbosa
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
