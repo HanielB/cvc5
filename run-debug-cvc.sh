a391154ee3fd6081e465f9026beec8306fcccad7
Haniel Barbosa <hanielbbarbosa@gmail.com>
2019-06-26 18:40:16 -0500

    more stuff

--- run-debug-cvc.sh
+++ run-debug-cvc.sh
@@ -1,1 +1,9 @@
 ./debug/bin/cvc4 --proof --dump-proof --no-unconstrained-simp --simplification=none --no-repeat-simp --no-global-negate --no-symmetry-breaker --no-pb-rewrites --no-sort-inference --no-pre-skolem-quant --no-bv-eq --no-bv-ineq --no-bv-algebraic --no-bv-to-bool --bool-to-bv=off --no-bv-intro-pow2 --no-bitblast-aig  "$@"
+
+#### add after ./run-debug-cvc.sh
+
+# solved before theories kick in (only resolution at level 0)
+# qf-unsat-01-nary.smt2 --new-proof
+
+
+# qf-unsat-01-nary.smt2 --new-proof -d theory::explain
