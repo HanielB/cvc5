ed8980e2f24f07866b9b86d5d2c2256dd813fdd1
Haniel Barbosa <hanielbbarbosa@gmail.com>
2019-07-29 15:20:36 -0500

    whatever

--- allrun.sh
+++ allrun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-solver=~/cvc/wt-thf/prod/bin/cvc4
-options="--uf-ho --finite-model-find"
+solver=~/cvc/wt-thf/debug/bin/cvc4
+options="--uf-ho --full-saturate-quant"
 traces=""
 time=""
-ulimit="ulimit -S -t 2"
+ulimit="ulimit -S -t 1"
 file="test-unsound.txt"
 path="/home/hbarbosa/benchmarks/tptp/Problems/"
 
@@ -12,30 +12,30 @@ echo "Options: $options"
 echo "Traces: $traces"
 echo
 
-while read name; do
-    echo "$solver on $path$name";
-    ($ulimit; $time $solver $options $traces  "$path$name")
-    echo "=====================================";
-done < $file
+# while read name; do
+#     echo "$solver on $path$name";
+#     ($ulimit; $time $solver $options $traces  "$path$name")
+#     echo "=====================================";
+# done < $file
 
 # for name in ~/benchmarks/tptp/Problems/GRA/*.p; do
-# for name in $(find ~/benchmarks/tptp/Problems -name '*.p'); do
-#     # Ignoring TH1
-#     if grep -q "TH1" "$name"; then
-#         continue
-#     fi
-#     # Ignoring lambdas for now
-#     # if grep -q "\^.\[" "$name"; then
-#     #     continue
-#     # fi
-#     # if grep -q "\^\[" "$name"; then
-#     #     continue
-#     # fi
-#     # Only run on problems with thf constructs
-#     if grep -q TH0 "$name"; then
-#         # echo "$name";
-#         echo "$solver on $name";
-#         ($ulimit; $time $solver $options $traces "$name")
-#         echo "=====================================";
-#     fi
-# done
+for name in $(find ~/benchmarks/tptp/Problems -name '*.p'); do
+    # Ignoring TH1
+    if grep -q "TH1" "$name"; then
+        continue
+    fi
+    # Ignoring lambdas for now
+    # if grep -q "\^.\[" "$name"; then
+    #     continue
+    # fi
+    # if grep -q "\^\[" "$name"; then
+    #     continue
+    # fi
+    # Only run on problems with thf constructs
+    if grep -q TH0 "$name"; then
+        # echo "$name";
+        echo "$solver on $name";
+        ($ulimit; $time $solver $options $traces "$name")
+        echo "=====================================";
+    fi
+done
