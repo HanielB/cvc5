abf7ee5b5498d2e625dc3b9ca3dd7f07af24ce37
Haniel Barbosa <hanielbbarbosa@gmail.com>
2020-03-31 16:56:46 -0300

    updating script and handwritten proof

--- qf-unsat-10-ite.lean
+++ qf-unsat-10-ite.lean
@@ -1,8 +1,8 @@
-import sigs.mysmt
+import sigs.hb_smt
 
 open mysmt
 
-open mysmt.sort mysmt.myterm mysmt.myformula
+open mysmt.sort mysmt.term
 
 @[simp] def A := atom "A"
 
@@ -10,20 +10,31 @@ open mysmt.sort mysmt.myterm mysmt.myformula
 @[simp] def b := const "b" A
 @[simp] def p := const "p" (mkArrowN [A, boolsort])
 
-@[simp] def pa := mkPapp p a
+@[simp] def pa := mkApp p a
 @[simp] def npa := mkNot pa
 
 @[simp] def iteterm := mkIte pa b a
 @[simp] def eqbite := mkEq b iteterm
 @[simp] def neqbite := mkNot eqbite
+@[simp] def eqaite := mkEq a iteterm
+@[simp] def neqaite := mkNot eqaite
+@[simp] def itedef := mkIte pa eqbite eqaite
+@[simp] def nitedef := mkNot itedef
 
-@[simp] def imp0 := mkImplies pa eqbite
+#eval itedef
+#eval nitedef
+#eval [nitedef, npa, eqbite]
+
+#eval mkNot itedef
+
+#eval iteterm
 
 noncomputable theorem teo :
     th_holds ([pa]) -> th_holds ([neqbite]) -> th_holds ([]) :=
 assume s0 : th_holds ([pa]),
 assume s1 : th_holds ([neqbite]),
-have s2   : th_holds ([imp0]), from @ite_intro_0 iteterm,
-have s3   : th_holds ([npa, eqbite]), from cnf_implies s2,
-have s4   : th_holds ([eqbite]), from fo_R s0 s3 pa,
-show th_holds [], from fo_R s4 s1 eqbite
+have s2   : th_holds ([itedef]), from @ite_intro iteterm,
+have s3   : th_holds ([nitedef, npa, eqbite]), from cnf_ite_pos_0,
+have s4   : th_holds ([npa, eqbite]), from fo_R s2 s3 itedef,
+have s5   : th_holds ([eqbite]), from fo_R s0 s4 pa,
+show th_holds [], from fo_R s5 s1 eqbite
--- run-debug-cvc.sh
+++ run-debug-cvc.sh
@@ -1,13 +1,11 @@
 ./debug/bin/cvc4 --dump-proof --no-expand-definitions-pp --no-unconstrained-simp --produce-unsat-cores --no-apply-subst --no-static-learning --simplification=none --no-repeat-simp --no-global-negate --no-symmetry-breaker --no-pb-rewrites --no-theory-preprocess --no-sort-inference --no-pre-skolem-quant --no-bv-eq --no-bv-ineq --no-bv-algebraic --no-bv-to-bool --bool-to-bv=off --no-bv-intro-pow2 --no-bitblast-aig  "$@"
 
+# to get rid of "unsat being in the proof file"
+# | tail -n+2 > qf-unsat-01-nary_cvc4.lean
+
+
+
+##### USAGE
 #### add after ./run-debug-cvc.sh
 
-# solved before theories kick in (only resolution at level 0)
-# qf-unsat-01-nary.smt2 --new-proof
-
-
 # qf-unsat-01-nary.smt2 --new-proof -d theory::explain
-
-
-
-# set args  --new-proof --dump-proof --no-expand-definitions-pp --no-unconstrained-simp --produce-unsat-cores --no-apply-subst --no-static-learning --simplification=none --no-repeat-simp --no-global-negate --no-symmetry-breaker --no-pb-rewrites --no-rewrite-apply-to-const --no-theory-preprocess --no-sort-inference --no-pre-skolem-quant --no-bv-eq --no-bv-ineq --no-bv-algebraic --no-bv-to-bool --bool-to-bv=off --no-bv-intro-pow2 --no-bitblast-aig  ~/cvc/wt-tbpf/qf-unsat-01-nary.smt2 -d newproof::pm -d newproof::sat -d cnf
