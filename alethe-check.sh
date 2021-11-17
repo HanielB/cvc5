969817d68866e45c9d2d47a687208ca7be902763
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-11-17 17:45:17 -0300

    minor

--- alethe-check.sh
+++ alethe-check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-./prod/bin/cvc5 --proof-format-mode=alethe --dag-thresh=0 $@ > tmp.alethe
+# ./prod/bin/cvc5 --proof-format-mode=alethe --dag-thresh=0 $@ > tmp.alethe
+./debug/bin/cvc5 --proof-format-mode=alethe --dag-thresh=0 $@ > tmp.alethe
 tail -n +2 tmp.alethe > pf.alethe
 alethe-proof-checker check pf.alethe $1
