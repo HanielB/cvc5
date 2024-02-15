1d263385ff7eff0174cccbcdf900bc2c26108487
Haniel Barbosa <hanielbbarbosa@gmail.com>
2024-02-15 08:45:37 -0300

    minor

--- alethe-check.sh
+++ alethe-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # tfile="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
-tfile="/home/hbarbosa/cvc/wt-proofnew/pf.alethe"
+tfile="/home/hbarbosa/cvc5/wt-proofnew/pf.alethe"
 
-./prod/bin/cvc5 --proof-format-mode=alethe --proof-granularity=theory-rewrite --lang=smt2 --dump-proofs --proof-alethe-res-pivots --dag-thresh=0  $@ | tail -n +2 > $tfile
-carcara check --allow-int-real-subtyping --expand-let-bindings --skip-unknown-rules $tfile $1
+./prod/bin/cvc5 --proof-format-mode=alethe --proof-granularity=theory-rewrite --lang=smt2 --dump-proofs  $@ | tail -n +2 > $tfile
+~/carcara/target/release/carcara check --allow-int-real-subtyping --expand-let-bindings $tfile $1
