5e80e14eeace8f8ee42876f8215c575d4d552f52
Haniel Barbosa <hanielbbarbosa@gmail.com>
2024-03-02 02:35:54 -0300

    utils for running

--- alethe-check.sh
+++ alethe-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # tfile="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
-tfile="/home/hbarbosa/cvc/wt-proofnew/pf.alethe"
+tfile="/home/hbarbosa/cvc5/wt-alethe/test.alethe"
 
-./prod/bin/cvc5 --proof-format-mode=alethe --proof-granularity=theory-rewrite --lang=smt2 --dump-proofs --proof-alethe-res-pivots --dag-thresh=0  $@ | tail -n +2 > $tfile
-carcara check --allow-int-real-subtyping --expand-let-bindings --skip-unknown-rules $tfile $1
+./prod/bin/cvc5 --proof-format=alethe --lang=smt2 --dump-proofs --dag-thresh=0  $@ | tail -n +2 > $tfile
+~/carcara/target/release/carcara check --no-print-with-sharing --allow-int-real-subtyping --expand-let-bindings -i $tfile $1
