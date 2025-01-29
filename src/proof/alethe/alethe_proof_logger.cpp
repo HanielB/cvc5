43ed6059750d53467ebe357b28591affda9ec7a1
Haniel Barbosa <hanielbbarbosa@gmail.com>
2025-01-29 14:33:24 -0300

    draft alethe proof logger

---
+++ src/proof/alethe/alethe_proof_logger.cpp
@@ -0,0 +1,155 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Haniel Barbosa
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Alethe proof logger utility.
+ */
+
+#include "proof/alethe/alethe_proof_logger.h"
+
+#include "proof/proof.h"
+#include "proof/proof_node_manager.h"
+#include "smt/proof_manager.h"
+
+namespace cvc5::internal {
+
+AletheProofLogger::AletheProofLogger(Env& env,
+                                     std::ostream& out,
+                                     smt::PfManager* pm,
+                                     smt::Assertions& as,
+                                     smt::ProofPostprocess* ppp)
+    : ProofLogger(env),
+      d_out(out),
+      d_pm(pm),
+      d_pnm(pm->getProofNodeManager()),
+      d_as(as),
+      d_ppp(ppp),
+      d_anc(nodeManager(), options().proof.proofAletheDefineSkolems),
+      d_appproc(env, d_anc),
+      d_apprinter(end, d_anc)
+
+{
+  Trace("alethe-pf-log-debug") << "Make Alethe proof logger" << std::endl;
+  if (logicInfo().isHigherOrder())
+  {
+    out << "(error \"Proof unsupported by Alethe: contains higher-order elements\")";
+    d_error = true;
+  }
+}
+
+AletheProofLogger::~AletheProofLogger() {}
+
+void AletheProofLogger::printPfNodeAlethe(std::shared_ptr<ProofNode> pfn)
+{
+  options::ProofCheckMode oldMode = options().proof.proofCheck;
+  d_pnm->getChecker()->setProofCheckMode(options::ProofCheckMode::NONE);
+  std::map<Node, std::string> assertionNames;
+  if (d_appproc.process(pfn))
+  {
+    d_apprinter.print(out, pfn, assertionNames);
+  }
+  else
+  {
+    out << "(error " << d_apprinter.getError() << ")";
+  }
+  d_pnm->getChecker()->setProofCheckMode(oldMode);
+}
+
+void AletheProofLogger::logCnfPreprocessInputs(const std::vector<Node>& inputs)
+{
+  if (!d_error)
+  {
+    return;
+  }
+  Trace("alethe-pf-log") << "; log: cnf preprocess input proof start"
+                         << std::endl;
+  CDProof cdp(d_env);
+  Node conc = nodeManager()->mkAnd(inputs);
+  cdp.addTrustedStep(conc, TrustId::PREPROCESSED_INPUT, inputs, {});
+  std::shared_ptr<ProofNode> pfn = cdp.getProofFor(conc);
+  ProofScopeMode m = ProofScopeMode::DEFINITIONS_AND_ASSERTIONS;
+  d_ppProof = d_pm->connectProofToAssertions(pfn, d_as, m);
+  printPfNodeAlethe(d_ppProof);
+  Trace("alethe-pf-log") << "; log: cnf preprocess input proof end"
+                         << std::endl;
+}
+
+void AletheProofLogger::logCnfPreprocessInputProofs(
+    std::vector<std::shared_ptr<ProofNode>>& pfns)
+{
+  Trace("alethe-pf-log") << "; log: cnf preprocess input proof start" << std::endl;
+  // if the assertions are empty, we do nothing. We will answer sat.
+  std::shared_ptr<ProofNode> pfn;
+  if (!pfns.empty())
+  {
+    if (pfns.size() == 1)
+    {
+      pfn = pfns[0];
+    }
+    else
+    {
+      pfn = d_pnm->mkNode(ProofRule::AND_INTRO, pfns, {});
+    }
+    ProofScopeMode m = ProofScopeMode::DEFINITIONS_AND_ASSERTIONS;
+    d_ppProof = d_pm->connectProofToAssertions(pfn, d_as, m);
+    printPfNodeAlethe(d_ppProof);
+  }
+  Trace("alethe-pf-log") << "; log: cnf preprocess input proof end" << std::endl;
+}
+
+void AletheProofLogger::logTheoryLemmaProof(std::shared_ptr<ProofNode>& pfn)
+{
+  Trace("alethe-pf-log") << "; log theory lemma proof start " << pfn->getResult()
+                  << std::endl;
+  d_lemmaPfs.emplace_back(pfn);
+  printPfNodeAlethe(pfn);
+  Trace("alethe-pf-log") << "; log theory lemma proof end" << std::endl;
+}
+
+void AletheProofLogger::logTheoryLemma(const Node& n)
+{
+  Trace("alethe-pf-log") << "; log theory lemma start " << n << std::endl;
+  std::shared_ptr<ProofNode> ptl =
+      d_pnm->mkTrustedNode(TrustId::THEORY_LEMMA, {}, {}, n);
+  d_lemmaPfs.emplace_back(ptl);
+  printPfNodeAlethe(ptl);
+  Trace("alethe-pf-log") << "; log theory lemma end" << std::endl;
+}
+
+void AletheProofLogger::logSatRefutation()
+{
+  Trace("alethe-pf-log") << "; log SAT refutation start" << std::endl;
+  std::vector<std::shared_ptr<ProofNode>> premises;
+  // TODO I think this should break with Alethe
+  Assert(d_ppProof->getRule() == ProofRule::SCOPE);
+  Assert(d_ppProof->getChildren()[0]->getRule() == ProofRule::SCOPE);
+  std::shared_ptr<ProofNode> ppBody =
+      d_ppProof->getChildren()[0]->getChildren()[0];
+  premises.emplace_back(ppBody);
+  premises.insert(premises.end(), d_lemmaPfs.begin(), d_lemmaPfs.end());
+  Node f = nodeManager()->mkConst(false);
+  std::shared_ptr<ProofNode> psr =
+      d_pnm->mkNode(ProofRule::SAT_REFUTATION, premises, {}, f);
+  printPfNodeALethe(psr);
+  Trace("alethe-pf-log") << "; log SAT refutation end" << std::endl;
+}
+
+void AletheProofLogger::logSatRefutationProof(std::shared_ptr<ProofNode>& pfn)
+{
+  Trace("alethe-pf-log") << "; log SAT refutation proof start" << std::endl;
+  // connect to preprocessed
+  std::shared_ptr<ProofNode> spf =
+      d_pm->connectProofToAssertions(pfn, d_as, ProofScopeMode::NONE);
+  printPfNodeALethe(spf);
+  Trace("alethe-pf-log") << "; log SAT refutation proof end" << std::endl;
+}
+
+}  // namespace cvc5::internal
---
+++ src/proof/alethe/alethe_proof_logger.h
@@ -0,0 +1,90 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Haniel Barbosa
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Alethe proof logger utility.
+ */
+
+#include "cvc5_private.h"
+
+#ifndef CVC5__PROOF__ALETHE__ALETHE_PROOF_LOGGER_H
+#define CVC5__PROOF__ALETHE__ALETHE_PROOF_LOGGER_H
+
+#include "proof/alethe/alethe_let_binding.h"
+#include "proof/alethe/alethe_node_converter.h"
+#include "proof/alethe/alethe_post_processor.h"
+#include "proof/alethe/alethe_printer.h"
+#include "smt/proof_logger.h"
+
+namespace cvc5::internal {
+
+/**
+ * The implementation of a proof logger for Alethe proofs
+ */
+class AletheProofLogger : public ProofLogger
+{
+ public:
+  /** */
+  AletheProofLogger(Env& env,
+                    std::ostream& out,
+                    smt::PfManager* pm,
+                    smt::Assertions& as,
+                    smt::ProofPostprocess* ppp);
+  ~AletheProofLogger();
+  /** Log preprocessing input */
+  void logCnfPreprocessInputs(const std::vector<Node>& inputs) override;
+  /** Log preprocessing input proof */
+  void logCnfPreprocessInputProofs(
+      std::vector<std::shared_ptr<ProofNode>>& pfns) override;
+  /** Log theory lemma */
+  void logTheoryLemma(const Node& n) override;
+  /** Log theory lemma proof */
+  void logTheoryLemmaProof(std::shared_ptr<ProofNode>& pfn) override;
+  /** Log SAT refutation */
+  void logSatRefutation() override;
+  /** Log SAT refutation proof */
+  void logSatRefutationProof(std::shared_ptr<ProofNode>& pfn) override;
+
+  /** Translate the proof node to Alethe and print it. */
+  void printPfNodeAlethe(std::shared_ptr<ProofNode> pfn);
+
+ private:
+  /** The output stream */
+  std::ostream& d_out;
+  /** Pointer to the proof manager, for connecting proofs to inputsw */
+  smt::PfManager* d_pm;
+  /** Pointer to the proof node manager */
+  ProofNodeManager* d_pnm;
+  /** Reference to the assertions of SMT solver */
+  smt::Assertions& d_as;
+  /** Pointer to the proof post-processor */
+  smt::ProofPostprocess* d_ppp;
+  /** The node converter, used for printing */
+  proof::AletheNodeConverter d_anc;
+  /** The Alethe proof post processor */
+  proof::AletheProofPostprocess* d_appproc;
+  /** The proof printer */
+  proof::AletheProofPrinter d_apprinter;
+  /** The preprocessing proof we were notified of, which we may have created */
+  std::shared_ptr<ProofNode> d_ppProof;
+  /**
+   * The list of theory lemma proofs we were notified of, which we may have
+   * created.
+   */
+  std::vector<std::shared_ptr<ProofNode>> d_lemmaPfs;
+
+  /** Whether there was an error for some logged proof. */
+  bool d_hadError;
+};
+
+}  // namespace cvc5::internal
+
+#endif /* CVC5__PROOF__PROOF_LOGGER_H */
